7bc9fe6a2b5b1efccd380949030a4a29a0933671
Wes-Skeeter <wesley.skeeter@alumni.ubc.ca>
2019-12-03 13:42:52 -0800

    Third

---
+++ DenseNet.py
@@ -0,0 +1,225 @@
+import numpy as np 
+import pandas as pd
+import math
+from sklearn import metrics
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from functools import partial
+import keras.backend as K
+from keras.models import model_from_json
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,normalize
+from sklearn.externals import joblib
+from sklearn import metrics
+from sklearn.utils import resample
+
+def Boot_Loss(y_true,y_pred):
+    return(K.sum(K.log(y_pred)+y_true/y_pred)/2)
+
+def Params(Func,target,MP = True,processes = 3):
+    params = {}
+    if MP == False:params['proc']=1
+    else:params['proc']=processes
+    if Func == 'Full':
+        K = 15
+        # splits_per_mod = 3
+    elif Func == 'Test':
+        K = 6
+        # splits_per_mod = 2
+    elif Func == 'Single':
+        K = 1
+        # splits_per_mod = 1
+    params['K'] = K
+    params['epochs'] = 1000
+    params['target'] = target
+    # params['splits_per_mod'] = splits_per_mod
+    params['Save'] = {}
+    params['Save']['Weights']=True
+    params['Save']['Model']=True
+    params['Loss']='mean_squared_error'
+    params['Memory']=.3
+    params['validation_split'] = .2
+    params['iteration'] = 1
+    params['Verbose'] = 0
+    params['Eval'] = True
+    params['patience']=10
+    return(params)
+
+def Dense_Model(params,inputs,lr=1e-4):
+    import keras
+    from keras.models import Sequential
+    from keras.layers import Dense, Activation, Dropout
+    from keras.wrappers.scikit_learn import KerasRegressor
+    from keras.callbacks import EarlyStopping,ModelCheckpoint,LearningRateScheduler
+    import tensorflow as tf
+    from keras.constraints import nonneg
+    # patience=10
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = params['Memory']
+    session = tf.Session(config=config)
+    model = Sequential()#'relu'
+    NUM_GPU = 1 # or the number of GPUs available on your machin
+    adam = keras.optimizers.Adam(lr = lr)
+    gpu_list = []
+    initializer = keras.initializers.glorot_uniform(seed=params['iteration'])
+    print(params['Save']['Weights'])
+    for i in range(NUM_GPU): gpu_list.append('gpu(%d)' % i)
+    if params['Loss'] == 'Boot_Loss':
+        model.add(Dense(params['N'], input_dim=inputs,activation='relu',kernel_initializer=initializer,kernel_constraint=nonneg()))
+        model.add(Dense(1,activation='elu',kernel_constraint=nonneg()))
+        model.compile(loss=Boot_Loss, optimizer='adam')
+    else:
+        model.add(Dense(params['N'], input_dim=inputs,activation='relu',kernel_initializer=initializer))
+        model.add(Dropout(0.1))
+        model.add(Dense(int(params['N']/2), activation='relu'))
+        model.add(Dense(1))
+        model.compile(loss=params['Loss'], optimizer='adam')#,context=gpu_list) # - Add if using MXNET
+    if params['Save']['Weights'] == True:
+        callbacks = [EarlyStopping(monitor='val_loss', patience=params['patience'],verbose=params['Verbose']),
+             ModelCheckpoint(filepath=params['Spath']+params['Sname']+str(params['iteration'])+'.h5', monitor='val_loss', save_best_only=True)]
+    else:
+        callbacks = [EarlyStopping(monitor='val_loss', patience=params['patience'])]
+    return(model,callbacks)
+
+def Train_DNN(params,X_train,y_train,X,y):#,X_fill):X_test,y_test,
+    epochs = params['epochs']
+    np.random.seed(params['iteration'])
+    from keras import backend as K
+    Mod,callbacks = Dense_Model(params,X_train.shape[1])
+    # print(Mod)
+    batch_size=int(y_train.shape[0]*.05)
+    Mod.fit(X_train, # Features
+            y_train, # Target vector
+            epochs=epochs, # Number of epochs
+            callbacks=callbacks, # Early stopping
+            verbose=params['Verbose'], # Print description after each epoch
+            batch_size=batch_size, # Number of observations per batch
+            # validation_data=(X_test, y_test),# Data for evaluation
+            validation_split=params['validation_split']) # Validation Fracton
+    # X_train = np.append(X_train,X_test,axis=0)
+    Y_target = Mod.predict(X,batch_size = batch_size)
+    if params['Save']['Model'] == True:
+        model_json = Mod.to_json()
+        with open(params['Spath']+params['Sname']+".json", "w") as json_file:
+            json_file.write(model_json)
+    return(Y_target)#,y_val,Rsq)
+
+def TTV_Split(iteration,params,X,y):
+    params['iteration'] = iteration
+    if params['iteration']>0 and params['Loss'] != 'Variance_Loss' and params['Loss']!= 'Boot_Loss':
+        params['Save']['Model'] = False
+    indicies = np.arange(0,y.shape[0],dtype=int)
+    ones = np.ones(y.shape[0],dtype=int)
+    X_train,X_test,y_train,y_test,i_train,i_test,ones_train,ones_test=train_test_split(X,y,indicies,ones, test_size=.3, random_state=params['iteration'])#0.2s
+
+    Y_hat=Train_DNN(params,X,y,X_test,y_test)
+    y_true = np.append(y_train,y_test,axis=0)
+    X_true = np.append(X_train,X_test,axis=0)
+    index = np.append(i_train,i_test,axis=0)
+    ones = np.append(ones_train,ones_test)
+    return(Y_hat,y_true,X_true,index,ones)
+
+
+def Bootstrap(iteration,params,X,y):
+    params['iteration']=iteration
+    np.random.seed(params['iteration'])
+    ones = np.ones(y.shape[0],dtype=int)
+    indicies = np.arange(0,y.shape[0],dtype=int)
+    X_train,y_train = resample(X,y, n_samples=y.shape[0])
+    Test = np.array([i for i,x in zip(indicies,y) if x.tolist() not in y_train.tolist()]) 
+    ones[Test] *= 0
+    Y_hat=Train_DNN(params,X_train,y_train,X,y)
+    return(Y_hat,y,X,ones)
+
+def Calculate_Var(params,Y_hat_train,Y_hat_val,y_true,X_true,count_train,count_val):
+    Y_hat_train_bar=np.nanmean(Y_hat_train,axis=0)
+    Y_hat_val_bar=np.nanmean(Y_hat_val,axis=0)
+
+    Y_hat_train_var = 1/(np.nansum(count_train)-1)*np.nansum((Y_hat_train - Y_hat_train_bar)**2,axis=0)
+    Y_hat_val_var = 1/(np.nansum(count_val)-1)*np.nansum((Y_hat_val - Y_hat_val_bar)**2,axis=0)
+    r2_train = np.maximum((y_true[0,:]-Y_hat_train_bar)**2-Y_hat_train_var,0)
+    r2_val = np.maximum((y_true[0,:]-Y_hat_val_bar)**2-Y_hat_val_var,0)
+
+
+    params['Loss'] = 'Boot_Loss'
+    params['Validate'] = False
+    params['Sname'] = 'Var'
+    params['Save']['Model'] = True
+
+    y = r2_val
+    Valid = np.where(np.isnan(y)==False)
+    y = y[Valid]
+    X = X_true[Valid]
+    YStandard = MinMaxScaler(feature_range=(.1, 1))
+    # YStandard = StandardScaler()
+    # XStandard = StandardScaler()
+    YScaled = YStandard.fit(y.reshape(-1, 1))
+    XStandard = joblib.load(params['Spath']+'X_scaler.save') 
+    XScaled = XStandard.fit(X)#.reshape(-1, 1))
+    y = YScaled.transform(y.reshape(-1, 1))
+    X = XScaled.transform(X)
+
+
+    scaler_filename = "YVar_scaler.save"
+    joblib.dump(YStandard, params['Spath']+scaler_filename) 
+    scaler_filename = "XVar_scaler.save"
+    joblib.dump(XStandard, params['Spath']+scaler_filename) 
+    # print('Var!!')
+    init=1#int(np.random.rand(1)[0]*100)
+    # Y_hat_var,y_true_var,X_true_var,index_var,ones_var = TTV_Split(init,params,X,y)
+
+    params['iteration'] = 0
+    Y_hat_var=Train_DNN(params,X,y,X,y)
+    Y_hat_var = YScaled.inverse_transform(Y_hat_var.reshape(-1,1))
+    y_true_var = YScaled.inverse_transform(y.reshape(-1,1))
+    # print(Y_hat_var,Y_hat_var.shape)
+    MSE = []
+    if params['Eval'] == True:
+        for i in range(params['K']):
+            try:
+                Test = pd.DataFrame(data={'target':Y_hat_val[i],'y':y_true[i]}).dropna()
+                # print(Y_hat_val,y_true)
+                MSE.append(metrics.mean_squared_error(Test['target'],Test['y']))
+            except:
+                print('No Go'+str(i))
+                pass
+    MSE = np.asanyarray(MSE)
+    Test = pd.DataFrame(data={'target':Y_hat_val_bar,'y':y_true[i]}).dropna()
+    # print(Y_hat_val_bar.shape,y_true.mean(axis=0).shape)
+    # MSE.append(metrics.mean_squared_error(Test['target'],Test['y']))
+    mse = metrics.mean_squared_error(Test['y'],Test['target'])
+    r2 = metrics.r2_score(Test['y'],Test['target'])
+    SE = (((MSE-mse)**2).sum()/(params['K']-1))**.5
+    return(mse,SE,r2)
+
+def Sort_outputs(k,params,Y_hat,y_true,X_true,ones):
+    ones_train = ones+0.0
+    ones_val = ones*-1+1.0
+    count_train = ones_train
+    count_val = ones_val
+    ones_train[ones_train==0] = np.nan
+    ones_val[ones_val==0] = np.nan
+    Y_hat_train = Y_hat.copy()*ones_train
+    Y_hat_val = Y_hat.copy()*ones_val
+    y_true2 = y_true.copy()
+    X_true2 = X_true.copy()
+    return(Calculate_Var(params,Y_hat_train,Y_hat_val,y_true2,
+               X_true2[0,:,],count_train,count_val))#,ones_train,ones_val)
+
+
+def Load_Model(i,X,params):
+    json_file = open(params['Spath']+params['Sname']+'.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # return(loaded_model)
+
+# def Load_Weights(loaded_model,params):
+    loaded_model.load_weights(params['Spath']+params['Sname']+str(i)+'.h5')
+    if params['Loss'] =='Boot_Loss':
+        loaded_model.compile(loss=Boot_Loss, optimizer='adam')
+    elif params['Loss']=='Variance_Loss':
+        loaded_model.compile(loss=Variance_Loss, optimizer='adam')
+    else:
+        loaded_model.compile(loss=params['Loss'], optimizer='adam')
+
+    return(loaded_model.predict(X).reshape(-1,1))
---
+++ ReadStandardTimeFill.py
@@ -0,0 +1,104 @@
+
+import numpy as np
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler, Normalizer, MinMaxScaler
+from sklearn.externals import joblib
+import os
+
+class ReadStandardTimeFill:#[1e-6 * 44.0095 *3600,1e-3 * 16.04246 *3600]
+    def __init__(self,params,Name,CombineKeys=[],Conversions=[1,1e3],resample=None):
+        self.Master = pd.read_csv(params['Dpath']+Name,delimiter = ',',header = 0,na_values = -9999)
+        self.Master.loc[self.Master['VWC']<.5,'VWC']=np.nan
+        self.Master = self.Master.set_index(pd.DatetimeIndex(pd.to_datetime(self.Master['datetime'])))
+        self.Master['DOY2'] = self.Master.index.dayofyear*1.0
+        self.Master['HR'] = self.Master.index.hour*1.0
+        self.Master['fco2'] *= Conversions[0]
+        self.Master['ER'] *= Conversions[0]
+        self.Master['fch4'] *= Conversions[1]
+        self.params=params
+        if len(CombineKeys) >0:
+            for i in range(0,len(CombineKeys),2):
+                self.Master[CombineKeys[i]]=self.Master[CombineKeys[i+1]].sum(axis=1)
+        self.TimeSteps=0
+        if resample != None:
+            self.Master=self.Master.resample(resample).mean()
+        
+    def Scale(self,y_var,X_vars,ScalePath = None,Project=False,fillTarget=None):
+        self.y_var = y_var
+        if Project == False:
+            self.Data = self.Master[np.isfinite(self.Master[y_var])]
+            self.y = self.Data[y_var].values
+            self.Ytru = self.y+0.0
+        self.Data = self.Data.interpolate().bfill()
+        self.Data = self.Data.interpolate().ffill()
+        if ScalePath is None:
+            YStandard = StandardScaler()
+            # YStandard = Normalizer()
+            self.YScaled = YStandard.fit(self.y.reshape(-1, 1))
+            Yscale = self.YScaled.transform(self.y.reshape(-1, 1))
+            self.y = np.ndarray.flatten(Yscale)
+        else:
+            self.YScaled = joblib.load(ScalePath+'Y_scaler.save') 
+            self.YvarScaled = joblib.load(ScalePath+'YVar_scaler.save')
+            # Yscale = self.YScaled.transform(self.y.reshape(-1, 1))
+            # self.y = np.ndarray.flatten(Yscale)
+        X = self.Data[X_vars]
+        self.input_shape = len(X_vars)
+        if ScalePath is None:
+            XStandard = StandardScaler()
+            # XStandard = Normalizer()
+            self.XScaled = XStandard.fit(X)
+        else:
+            self.XScaled = joblib.load(ScalePath+'X_scaler.save') 
+        # self.XScaled = XStandard.fit(X)
+        self.X = self.XScaled.transform(X)
+        if fillTarget is None:
+            Filling = self.Master[X_vars]
+        else:
+            Filling = fillTarget[X_vars]
+
+        Filling = Filling.interpolate().bfill()
+        Filling = Filling.interpolate().ffill()
+        #XStandard = StandardScaler()
+        #self.XFillScaled= XStandard.fit(Filling)
+        self.X_fill = self.XScaled.transform(Filling)
+        if ScalePath is None:
+            try:
+                os.mkdir(self.params['Spath'])
+            except:
+                pass
+            scaler_filename = "Y_scaler.save"
+            joblib.dump(self.YScaled, self.params['Spath']+scaler_filename) 
+            scaler_filename = "X_scaler.save"
+            joblib.dump(self.XScaled, self.params['Spath']+scaler_filename)
+
+        
+    def TimeShape(self,rolls):
+        X1 = self.X
+        Xf = self.X_fill
+        self.X_time = np.zeros(shape = (X1.shape[0],rolls+1,X1.shape[1]))
+        self.X_time[:,0,:] = X1
+        self.X_ftime = np.zeros(shape = (Xf.shape[0],rolls+1,Xf.shape[1]))
+        self.X_ftime[:,0,:] = Xf
+        if rolls > 0:
+            for roll in range(0,rolls):
+                X2 = np.roll(X1,(roll+1),axis=0)
+                X2f = np.roll(Xf,(roll+1),axis=0)
+                self.X_time[:,roll+1,:] = X2
+                self.X_ftime[:,roll+1,:] = Xf
+        self.X_time = self.X_time[rolls+1:,:,:]
+        self.X_ftime = self.X_ftime[rolls+1:,:,:]
+        self.y_time = self.y[rolls+1:]
+        self.y_ftime = self.y[rolls+1:]
+        self.TimeSteps = rolls+1
+        
+    def Fill(self,Y_Pred,Name):
+        Y_fill = self.YScaled.inverse_transform(Y_Pred.reshape(-1,1))
+        if self.TimeSteps>0:
+            nanz = np.zeros(shape=(self.TimeSteps,1))
+            nanz[:,:] = np.nan
+            Y_Pred = np.concatenate((nanz,Y_fill),axis=0).reshape(-1,1)
+        else:
+            Y_Pred = Y_fill
+        self.Master['TempFill'] = Y_Pred
+        self.Master[Name] = self.Master[self.y_var].fillna(self.Master['TempFill'])
